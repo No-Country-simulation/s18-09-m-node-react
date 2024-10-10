3c44504459a9a449e6abb104c7cf8f2a26ee3697
Benjamín Peyraga <benjapey99@gmail.com>
2024-10-09 23:42:31 -0300

    Nodemailer para Registro de Usario.

--- backend/src/entity.users/mailer.ts
+++ backend/src/entity.users/mailer.ts
@@ -9,23 +9,69 @@ const transporter = nodemailer.createTransport({
     },
 });
 
-export const sendMail = async (email: string, password: string) => {
+export const sendMailRegister = async (email: string, password: string) => {
     const mailOptions = {
         from: EMAIL,
         to: email,
-        subject: 'Registro éxitoso en Break & Focus',
-        text: `Bienvenido a Break & Focus !!
-      Su registro ha sido exitoso. Por favor no comparta estos datos:
+        subject: 'Registro Exitoso en Break & Focus',
+        text: `¡Hola!
+    
+    Te damos la bienvenida a Break & Focus. Tu registro ha sido exitoso, y ahora puedes disfrutar de todos nuestros servicios.
+    
+    A continuación, encontrarás tus datos de acceso:
+    
+    Usuario: ${email}
+    Contraseña: ${password}
 
-      User: ${email}
-      Password: ${password}
-      
-      Este es un mail autogenerado, por favor no contestar.`
+    Si tienes alguna pregunta o necesitas ayuda, no dudes en contactarnos.
+    
+    ¡Gracias por unirte a Break & Focus!
+    
+    Atentamente,
+    El equipo de Break & Focus
+    
+    ---
+    Este es un correo autogenerado, por favor no respondas a este mensaje.`
     };
+    
 
     try {
         const info = await transporter.sendMail(mailOptions);
-        console.log('Correo enviado:', info.response);
+        console.log('Correo enviado:', info);
+    } catch (error) {
+        console.error('Error al enviar correo:', error);
+    }
+};
+
+export const sendMailForget = async (email: string, password: string) => {
+    const mailOptions = {
+        from: EMAIL,
+        to: email,
+        subject: 'Contraseña Restablecida - Break & Focus',
+        text: `Hola,
+    
+    Tu contraseña ha sido restablecida exitosamente. A continuación, te proporcionamos una nueva contraseña temporal para acceder a tu cuenta en Break & Focus:
+    
+    Usuario: ${email}
+    Contraseña temporal: ${password}
+    
+    Por seguridad, te recomendamos iniciar sesión de inmediato y cambiar esta contraseña por una de tu elección. Para hacerlo, ingresa a tu cuenta y dirígete a la sección de configuración.
+    
+    Si no solicitaste este cambio, por favor contáctanos de inmediato.
+    
+    Gracias por ser parte de Break & Focus.
+    
+    Atentamente,
+    El equipo de Break & Focus
+    
+    ---
+    Este es un correo autogenerado, por favor no respondas a este mensaje.`
+    };
+    
+
+    try {
+        const info = await transporter.sendMail(mailOptions);
+        console.log('Correo enviado:', info);
     } catch (error) {
         console.error('Error al enviar correo:', error);
     }
--- backend/src/entity.users/service/register.ts
+++ backend/src/entity.users/service/register.ts
@@ -1,5 +1,6 @@
 import { User, UserDocument, UserAttributes } from '../model';
-import { sendMail } from '../mailer'
+import { sendMailRegister } from '../mailer'
+
 
 export async function register(user: UserAttributes): Promise<UserDocument> {
   try {
@@ -14,10 +15,12 @@ export async function register(user: UserAttributes): Promise<UserDocument> {
     const newUser = new User(user);
     const registeredUser = await newUser.save();
 
-    if (registeredUser) {
-      await sendMail(registeredUser.email, registeredUser.password)
-    }
+
     if (!registeredUser) throw new Error("Unable to register user.");
+    if(registeredUser) {
+      const password = registeredUser.password
+      await sendMailRegister(registeredUser.email, password)
+    }
 
     return registeredUser;
   } catch (err) {
