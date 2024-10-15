684a5e16a489db1bc5b5a4c67334351f974ffd23
Lalomax <lalomax365@gmail.com>
2024-10-15 15:33:44 -0400

    feat: register from validation

--- frontend/src/app/register/page.tsx
+++ frontend/src/app/register/page.tsx
@@ -7,13 +7,14 @@ import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import { Separator } from "@/components/ui/separator"
-import { Facebook, Mail } from 'lucide-react'
+import { Facebook, Mail, Eye, EyeOff } from 'lucide-react'
 import Link from 'next/link'
 import { toast } from "sonner";
 
 export default function RegisterForm() {
   // const [email, setEmail] = useState('')
-  // const [password, setPassword] = useState('')
+  const [showPassword, setShowPassword] = useState(false)
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false)
   const [confirmPassword, setConfirmPassword] = useState('')
   const { fetchData } = useFetchData();
   const router = useRouter();
@@ -25,14 +26,39 @@ export default function RegisterForm() {
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault()
     // Handle form submission logic here
-    const response = await fetchData("registerUser", formState );
-    
+    const response = await fetchData("registerUser", formState);
+
     if (response.status) {
       router.push("/register/success")
     } else {
       toast.error("No se pudo crear el usuario");
     }
   }
+  const validatePassword = (password: string) => {
+    const errors = [];
+
+    if (password.length < 8) {
+      errors.push("La contraseña debe tener al menos 8 caracteres");
+    }
+
+    if (!/\d/.test(password)) {
+      errors.push("La contraseña debe tener al menos un número");
+    }
+
+    if (!/[A-Z]/.test(password)) {
+      errors.push("La contraseña debe tener al menos una mayúscula");
+    }
+
+    if (!/[a-z]/.test(password)) {
+      errors.push("La contraseña debe tener al menos una minúscula");
+    }
+
+    if (!/[ !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/.test(password)) {
+      errors.push("La contraseña debe tener al menos un carácter especial");
+    }
+
+    return errors.length > 0 ? errors[0] : undefined;
+  };
 
   return (
     <div className="h-screen flex items-center justify-center">
@@ -63,34 +89,56 @@ export default function RegisterForm() {
                 onChange={setFormState}
                 required
               />
+
             </div>
-            <div className="space-y-2">
+
+            {/* password */}
+
+            <div className="space-y-2 relative">
               <label htmlFor="password" className="text-sm font-medium">
                 Contraseña
               </label>
               <Input
                 id="password"
                 name="password"
-                type="password"
+                type={showPassword ? "text" : "password"}
                 placeholder="Ingrese su contraseña"
                 value={formState.password}
                 onChange={setFormState}
                 required
+                error={validatePassword(formState.password)}
               />
+              <button
+                className="absolute top-1/2 right-2 transform -translate-y-1/2"
+                onClick={() => setShowPassword(!showPassword)}
+              >
+                {showPassword ? <EyeOff /> : <Eye />}
+              </button>
             </div>
-            <div className="space-y-2">
+
+            {/* confirm password */}
+            
+            <div className="space-y-2 relative">
               <label htmlFor="confirmPassword" className="text-sm font-medium">
                 Repetir Contraseña
               </label>
               <Input
                 id="confirmPassword"
                 name="confirmPassword"
-                type="password"
+                type={showConfirmPassword ? "text" : "password"}
                 placeholder="Ingrese su contraseña"
                 value={confirmPassword}
                 onChange={(e) => setConfirmPassword(e.target.value)}
                 required
+                error={formState.password !== confirmPassword ? "Las contraseñas no coinciden" : undefined}
               />
+              <button
+                className="absolute top-1/2 right-2 transform -translate-y-1/2"
+                onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+              >
+                {showConfirmPassword ? <EyeOff /> : <Eye />}
+              </button>
+
             </div>
             <Button type="submit" className="w-full">
               Registrate
