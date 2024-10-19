8fd3f8a0dd762f8004803fddf614fae0a73789a8
Lalomax <lalomax365@gmail.com>
2024-10-19 16:26:03 -0400

    perf: run backend on localhost
    @everyone, se modifica la ruta del nodo para acceder a los datos del backend en local

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 // import { userStore } from "@/context/zustand";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BASE_API_URL || "http://localhost:3001/api/v0", // url que viene del .env o el local
+  baseURL: process.env.NEXT_PUBLIC_BASE_API_URL || "http://localhost:3001/v1", // url que viene del .env o el local
   headers: {
     "Content-Type": "application/json",
   },
