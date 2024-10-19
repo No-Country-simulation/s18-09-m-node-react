f0b807160d10cb068c30a4c147c93c55fb775d8d
MartinKun <martinkunbrc@hotmail.com>
2024-10-19 18:15:39 -0300

    feat: Charts was added to statistics page

--- frontend/src/app/statistics/charts/page.tsx
+++ frontend/src/app/statistics/charts/page.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import BarChart from "@/components/ui/charts/BarChart";
+import PieChart from "@/components/ui/charts/PieChart";
 import { Dashboard } from "@/components/ui/dashboard/Dashboard";
 
 const Charts: React.FC = () => {
@@ -16,7 +20,12 @@ const Charts: React.FC = () => {
   ];
   return (
     <Dashboard title="Estadísticas" items={items}>
-      <h1>Gráficos y visualizaciones</h1>
+      <div className="flex w-full pl-[120px]">
+        <PieChart />
+        <div className="-translate-x-[110px]">
+          <BarChart />
+        </div>
+      </div>
     </Dashboard>
   );
 };
