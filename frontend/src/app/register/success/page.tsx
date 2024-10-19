d6a066996f891d53ce394e8c33004d1de484c2f3
MartinKun <martinkunbrc@hotmail.com>
2024-10-18 23:02:51 -0300

    fix: updated styles for the successful registration page

--- frontend/src/app/register/success/page.tsx
+++ frontend/src/app/register/success/page.tsx
@@ -5,37 +5,20 @@ import React from "react";
 
 const RegisterSuccess: React.FC = () => {
   return (
-    <section
-      className="flex
-                 min-h-screen
-                 flex-col
-                 items-center
-                 justify-center
-                 bg-white p-4"
-    >
+    <section className="flex min-h-screen flex-col items-center justify-center bg-white p-4">
       <CheckIcon />
 
       <h1
-        className="mt-[9px]
-                   mb-[48px]
-                   w-[600px]
-                   text-center
-                   text-[40px]
-                   font-bold
-                   leading-[46px]"
+        className="mt-[11px] mb-12 w-full text-center text-[28px] font-bold leading-[34px] text-[#0859A3] 
+                   md:w-[600px] md:text-[40px] md:leading-[46px]"
       >
         ¡Registro exitoso!
       </h1>
 
-      <div className="mb-[90px]">
+      <div className="mb-12 md:mb-[90px]">
         <p
-          className="mb-[41px]
-                     max-w-[923px]
-                     text-center
-                     font-roboto
-                     text-2xl
-                     font-normal
-                     leading-[28.13px]"
+          className="mb-8 max-w-[340px] text-center font-roboto text-lg font-normal leading-6 
+                     md:mb-[41px] md:max-w-[762px] md:text-[24px] md:leading-[28.13px]"
         >
           Te enviamos un email para confirmar tu cuenta. Si no lo encuentras en
           tu bandeja de entrada, revisa en correo no deseado o spam.
@@ -43,24 +26,16 @@ const RegisterSuccess: React.FC = () => {
 
         <a
           href="#"
-          className="block w-full
-                     text-right
-                     no-underline
-                     text-[20px]
-                     text-2xl
-                     font-roboto
-                     leading-[63px]
-                     text-black
-                     font-[700]
-                     hover:text-blue-600
-                     transition-colors duration-300"
+          className="block w-full text-center no-underline text-[16px] font-bold leading-[24px] text-black 
+                     hover:text-blue-600 transition-colors duration-300 
+                     md:text-right md:text-[20px] md:leading-[28px]"
         >
           Reenviar email de confirmación
         </a>
       </div>
 
-      <div className="w-[454px]">
-        <Button asChild className="w-full">
+      <div className="w-full max-w-[300px] md:max-w-[454px]">
+        <Button asChild variant="customBlue" size="lg" className="w-full">
           <Link href="/">Ir a la página de inicio</Link>
         </Button>
       </div>
