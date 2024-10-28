5d13b715c06370e53d3bbe44509a4fc44bc44129
JoshVC <josuevalenzuela87@gmail.com>
2024-10-28 11:15:18 -0300

    fix/principal_home

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -52,7 +52,7 @@ export default function RootLayout({
         <Loader />
         <Toaster richColors />
         {children}
-        <Footer />
+        {/* <Footer /> */}
       </body>
     </html>
   );
