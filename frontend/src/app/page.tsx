b923c9067f4430fdb1bb62d336d119bfa3b888d1
Lalomax <lalomax365@gmail.com>
2024-10-29 14:14:41 -0400

    style: home responsive

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,22 +1,18 @@
 "use client";
-import Link  from "next/link";
+import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import HomeBrackground from "@/img/home.png";
 import Image from "next/image";
-import { useEffect} from "react";
-import { useMediaQuery } from 'react-responsive';
-import MobileLayout from "@/components/MobileLayout";
+import { useEffect } from "react";
 
 export default function Home() {
 
-  // const [isMobile, setIsMobile] = useState(false);
-  const isMobile = false;
-  const isMobileScreen = useMediaQuery({ query: '(max-width: 500px)' }); // Detect screens less than 400px wide
+  
   //Fondo personalizado
   useEffect(() => {
     const savedImage = localStorage.getItem("bgImage");
     const savedColor = localStorage.getItem("bgColor");
-  
+
     if (savedImage) {
       document.body.style.backgroundImage = `url(${savedImage})`;
       document.body.style.backgroundSize = "cover";
@@ -29,40 +25,45 @@ export default function Home() {
   }, []);
 
   return (
-    <div className=" relative min-h-screen">
-    {/* Conditionally render content based on screen size */}
-    {isMobileScreen ? (
-        <MobileLayout />
-      ) : (
-        <div>
-        <div className={`absolute inset-0 z-0 ${isMobile ? 'w-full h-[520px]' : 'w-[200vh] h-[520px] sm:w-full md:w-full md:h-full'}`}>
-          <Image
-            src={HomeBrackground}
-            alt="Oficina con persona trabajando"
-            layout="fill"
-            objectFit="cover"
-            quality={100}
-            priority
-          />
+    <div className="h-screen relative flex flex-col md:flex-row">
+      <div className="absolute inset-0 z-0 md:hidden">
+        <Image
+          src={HomeBrackground}
+          alt="Oficina con persona trabajando"
+          layout="fill"
+          objectFit="cover"
+          quality={100}
+          priority
+        />
+      </div>
+      <div className="flex-1 md:flex-none md:w-1/3">
+        <div className="relative z-10 max-w-md w-full p-6 text-center bg-white rounded-lg shadow-xl mx-auto md:mx-0 md:ml-10">
+          <h1 className="text-2xl md:text-3xl font-bold font-roboto text-tertiary">¡Bienvenido!</h1>
+          <p className="mb-4 text-gray-600 text-sm md:text-base">
+            Con <span className="font-bold">Break&Focus</span>, gestiona tu tiempo de forma inteligente,
+            <span className="font-bold">alternando entre trabajo concentrado y pausas activas.</span>
+          </p>
+          <p className="mb-6 text-gray-600 text-sm md:text-base">
+            ¿Empiezas hoy a trabajar mejor y sentirte bien?
+          </p>
+          <Link href="/home">
+            <Button className="w-full bg-emerald-500 hover:bg-emerald-600 text-white font-bold py-2 px-4 rounded">
+              Empezar Ahora
+            </Button>
+          </Link>
         </div>
-        <div className="relative z-10 max-w-md w-full p-6 text-center bg-white rounded-lg shadow-xl ml-4 md:ml-16 lg:ml-24 ">
-        <h1 className="text-3xl  font-bold font-roboto text-tertiary">¡Bienvenido!</h1>
-        <p className="mb-4 text-gray-600">
-        Con <span className="font-bold">Break&Focus</span>, gestiona tu tiempo de forma inteligente,
-        <span className="font-bold">alternando entre trabajo concentrado y pausas activas.</span>
-        </p>
-        <p className="mb-6 text-gray-600">
-          ¿Empiezas hoy a trabajar mejor y sentirte bien?
-        </p>
-        <Link href="/home">
-          <Button className="w-full bg-emerald-500 hover:bg-emerald-600 text-white font-bold py-2 px-4 rounded">
-            Empezar Ahora
-          </Button>
-        </Link>
       </div>
+      <div className="hidden md:flex-1 md:flex">
+        <Image
+          src={HomeBrackground}
+          alt="Oficina con persona trabajando"
+          layout="fill"
+          objectFit="cover"
+          quality={100}
+          priority
+        />
       </div>
-      )}
-    
-  </div> 
+    </div>
+
   );
 }
